c881b1f9ea0fb50842eba75f7c4f6ffeb4e12e1b
1Pulse <bonizulusa@gmail.com>
2025-03-22 12:41:23 +0200

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const User = require("./models/User");
+const User = require("/models/User"); // Adjust path based on your structure
 
 // Test Route
 router.get("/", (req, res) => {
